52bf9361a646154ebf80fe15689e87a95f2d7e89
Pete Looney <petelooney81@gmail.com>
2020-09-17 11:16:41 -0500

    comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const promise = new Promise((resolve, reject) => {
-    let randTime = Math.floor(Math.random() * 11)
+// Small ---
+// stopwatch create a simple stopwatch app that starts the timer and show the stopped time.
+const timer = new Promise((resolve, reject) => {
+    let randTime = Math.floor(Math.random() * 11) * 1000;
     setTimeout(()=>{
         resolve(randTime);
     }, randTime);
   });
   
-  promise.then(value => console.log(`${value} seconds`));
+  timer.then(value => console.log(`${value/1000} seconds`));
   
